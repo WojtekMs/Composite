9b02a2f86c87c5717060e31ac6e599dd5f121f0a
WojtekMs <wojtek.atmo@gmail.com>
2020-10-19 19:29:27 +0200

    examples of usaged added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+#include "Directory.hpp"
+#include "File.hpp"
+
 int main() {
-    
+    auto dir = std::make_shared<Directory>("dir");
+    auto file = std::make_shared<File>("file", 30);
+    dir->add(std::make_shared<Directory>("dir2"));    
+    dir->add(std::make_shared<File>("file1", 300));
+    Directory anotherDir("bigDir");
+    anotherDir.add(dir);
+    anotherDir.add(file);
+    dir->getSize();
+    file->getSize();
 }
