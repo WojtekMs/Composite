f02b61d00a2258e095a9fda87a0fa5add2f3d90a
WojtekMs <wojtek.atmo@gmail.com>
2020-10-19 16:05:02 +0200

    examples of usage added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+#include "Directory.hpp"
+#include "File.hpp"
+
 int main() {
-    
+    Directory dir("example");
+    dir.add(make_file("file1.txt", 150));
+    dir.add(make_file("file2.txt", 350));
+    File fil("bookaro");
+    fil.getSize();
+    dir.getSize();    
 }
